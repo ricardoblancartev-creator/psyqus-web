577eab8b9082700176188c62f7dff1ff234f83bd
Ricardo Blancarte Vilches <ricardoblancartev@gmail.com>
2026-02-17 19:36:05 -0600

    fix tailwind production build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
-import './globals.css' // <--- ESTO ES LO QUE FALTA
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Psyqus - Plataforma NOM-035",
+  description: "Plataforma SaaS para implementación de la NOM-035 en México",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es">
       <body>{children}</body>
     </html>
-  )
+  );
 }
+
