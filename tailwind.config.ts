304deee7fdd700c6053c877845b24d44af285039
Ricardo Blancarte Vilches <ricardoblancartev@gmail.com>
2026-03-15 14:26:12 -0600

    RESTORE: Recuperando diseño pro y login

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,11 +5,16 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#020617",
+        primary: "#06b6d4",
+      },
+    },
   },
   plugins: [],
 };
-
 export default config;
