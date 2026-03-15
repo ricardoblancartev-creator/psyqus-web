def09daea058b4a7ba820689a1a59c2766b84ed3
Ricardo Blancarte Vilches <ricardoblancartev@gmail.com>
2026-03-15 13:32:53 -0600

    feat: instalada openai y activado diseño tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Esto busca en TODAS las subcarpetas de app
+content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
