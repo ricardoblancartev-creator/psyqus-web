ef249bd821fc82f891839c06f7f87286d80a703e
Ricardo Blancarte Vilches <ricardoblancartev@gmail.com>
2026-03-17 23:15:38 -0600

    fix: corrigiendo import de componentes Clerk v6

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-﻿import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+﻿import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs/components";
 import Link from "next/link";
 import Image from 'next/image';
 
